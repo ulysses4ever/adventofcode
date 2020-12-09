8ae28e08acea35d482cff915fa6bdfcb9ab9c2d0
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2020-12-08 21:29:21 -0500

    day 8, part 1

--- day-8.hs
+++ day-8.hs
@@ -2,33 +2,35 @@
 {- cabal:
 build-depends: base, mtl, microlens-platform, pretty-simple
 -}
-{-# language LambdaCase, TemplateHaskell, BlockArguments #-}
+{-# language LambdaCase, TemplateHaskell, BlockArguments, MultiWayIf, RecordWildCards #-}
 {-# OPTIONS_GHC -Wall -O1 #-}
 module Main where
 
+import Control.Applicative
 import Control.Monad.Except
 import Control.Monad.State
 import Lens.Micro.Platform
 --import Text.Pretty.Simple
-import Data.Functor.Identity
 
 type Str = String
 type Acc = Int
 type PC = Int
 
 data St = S {
-  _ac :: Acc,
-  _pc :: PC,
-  _pcs :: [PC], -- ^ visited
+  _ac   :: Acc,
+  _pc   :: PC,
+  _pcs  :: [PC], -- ^ visited
+  _fork :: Bool,
   _prog :: [Str]}
   deriving Show
 $(makeLenses ''St)
 
-type Eval = State St ()
+type Eval = StateT St [] ()
 
 incPc :: Eval
 incPc = modify
-  \(S ac' pc' pcs' prog') -> S ac' (pc'+1) (pc':pcs') prog'
+  \(S ac' pc'     pcs'       fork' prog') ->
+    S ac' (pc'+1) (pc':pcs') fork' prog'
 
 acc, jmp, nop :: Int -> Eval
 acc n = modify $ over ac (+ n)
@@ -38,32 +40,41 @@ nop _ = pure ()
 control :: [Str]
 control = ["jmp"]
 
+mayFork :: Eval -> Eval
+mayFork cmd = do
+  S{..} <- get
+  unless _fork do
+    put S{_fork = True, ..}
+    cmd
+
 eval :: Str -> Eval
 eval s = let
   (cs, a) = case words s of
     [cs', as] -> (cs', (read . dropWhile (== '+')) $ as)
     _         -> error $ "Can't parse command: " ++ s
   in
-  a & case cs of -- I wish I had the real eval!
-    "acc" -> acc
-    "jmp" -> jmp
-    "nop" -> nop
+  case cs of -- I wish I had the real eval!
+    "acc" -> acc a
+    "jmp" -> jmp a <|> mayFork (nop a)
+    "nop" -> nop a <|> mayFork (jmp a)
     _     -> error $ "unknown command: " ++ cs
   >> unless (cs `elem` control) incPc
 
 evalLoop :: Eval
 evalLoop = do
-  (S _ac' pc' pcs' prog') <- get
-  unless (pc' `elem` pcs' || pc' >= length prog') do
-    eval $ prog' !! pc'
-    evalLoop
+  S{..} <- get
+  if | _pc `elem` _pcs     -> mzero
+     | _pc >= length _prog -> pure ()
+     | otherwise           -> do
+        eval $ _prog !! _pc
+        evalLoop
 
 main :: IO ()
 main = do
   inp <- readFile "input/day-8.txt"
   let
-    res1 = view (_2 . ac) . runIdentity . runStateT
-      evalLoop $ (S 0 0 [] $ lines inp)
+    res1 = view (_2 . ac) . head . runStateT
+      evalLoop $ (S 0 0 [] False $ lines inp)
     res2 = 2 :: Int
   print res1
   print res2
