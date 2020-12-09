10ad763a87c0e614663d6c846f3169d67a4b9533
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2020-12-08 23:11:41 -0500

    day-8, part 2: all hail mtl!

--- day-8.hs
+++ day-8.hs
@@ -1,16 +1,14 @@
 #!/usr/bin/env cabal
 {- cabal:
-build-depends: base, mtl, microlens-platform, pretty-simple
+build-depends: base, mtl, microlens-platform
 -}
-{-# language LambdaCase, TemplateHaskell, BlockArguments, MultiWayIf, RecordWildCards #-}
+{-# language TemplateHaskell, BlockArguments, MultiWayIf, RecordWildCards #-}
 {-# OPTIONS_GHC -Wall -O1 #-}
 module Main where
 
 import Control.Applicative
-import Control.Monad.Except
 import Control.Monad.State
 import Lens.Micro.Platform
---import Text.Pretty.Simple
 
 type Str = String
 type Acc = Int
@@ -19,32 +17,45 @@ type PC = Int
 data St = S {
   _ac   :: Acc,
   _pc   :: PC,
-  _pcs  :: [PC], -- ^ visited
-  _fork :: Bool,
-  _prog :: [Str]}
+  _pcs  :: [PC],  -- visited instructions
+  _fork :: Bool,  -- have we forked yet
+  _trc  :: [Str], -- trace executed instructions
+  _prog :: [Str]} -- our program
   deriving Show
+
 $(makeLenses ''St)
 
 type Eval = StateT St [] ()
 
+recordPc :: Eval
+recordPc = modify \s -> over pcs (view pc s :) s
+
 incPc :: Eval
-incPc = modify
-  \(S ac' pc'     pcs'       fork' prog') ->
-    S ac' (pc'+1) (pc':pcs') fork' prog'
+incPc = modify $ over pc (+1)
+
+trace' :: Str -> Int -> Eval
+trace' c n = modify $ over trc ((c ++ show n) :)
+trace'' :: Str -> Eval
+trace'' c = modify $ over trc (c :)
 
 acc, jmp, nop :: Int -> Eval
-acc n = modify $ over ac (+ n)
-jmp n = modify $ over pc (+ n)
-nop _ = pure ()
-
-control :: [Str]
-control = ["jmp"]
+acc n = do
+  trace' "acc " n
+  modify $ over ac (+ n)
+  incPc
+jmp n = do
+  trace' "jmp " n
+  modify $ over pc (+ n)
+nop n = do
+  trace' "nop " n
+  incPc
 
 mayFork :: Eval -> Eval
 mayFork cmd = do
   S{..} <- get
   unless _fork do
     put S{_fork = True, ..}
+    trace'' "FORK"
     cmd
 
 eval :: Str -> Eval
@@ -52,29 +63,35 @@ eval s = let
   (cs, a) = case words s of
     [cs', as] -> (cs', (read . dropWhile (== '+')) $ as)
     _         -> error $ "Can't parse command: " ++ s
-  in
-  case cs of -- I wish I had the real eval!
+  in do
+  recordPc
+  case cs of
     "acc" -> acc a
     "jmp" -> jmp a <|> mayFork (nop a)
     "nop" -> nop a <|> mayFork (jmp a)
     _     -> error $ "unknown command: " ++ cs
-  >> unless (cs `elem` control) incPc
 
 evalLoop :: Eval
 evalLoop = do
   S{..} <- get
-  if | _pc `elem` _pcs     -> mzero
-     | _pc >= length _prog -> pure ()
+
+  if | _pc `elem` _pcs     ->
+        trace'' "DIE" >>
+          mzero -- use `pure ()` instead to collect all traces
+
+     | _pc >= length _prog ->
+        trace'' "WIN" >> pure ()
+
      | otherwise           -> do
-        eval $ _prog !! _pc
-        evalLoop
+          -- trace'' "LOOP "
+          eval $ _prog !! _pc
+          evalLoop
 
 main :: IO ()
 main = do
   inp <- readFile "input/day-8.txt"
   let
-    res1 = view (_2 . ac) . head . runStateT
-      evalLoop $ (S 0 0 [] False $ lines inp)
-    res2 = 2 :: Int
-  print res1
+    res2 = view (_2 . ac) . head .
+      runStateT evalLoop $
+      (S 0 1 [] False [] $ "" : lines inp)
   print res2
