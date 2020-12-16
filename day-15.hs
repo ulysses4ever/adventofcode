9594226eb1715eb0295f6d8dd0ab2b5ccbe39efc
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2020-12-15 19:56:02 -0500

    day-15: Part 2: umpure vectors -- 20 secs :-(

--- day-15.hs
+++ day-15.hs
@@ -2,75 +2,75 @@
 {- cabal:
 build-depends: base, vector
 -}
-{-# language TypeApplications, RecordWildCards, StrictData, ViewPatterns #-}
+{-# language TypeApplications, RecordWildCards, StrictData, ViewPatterns, BlockArguments, NumericUnderscores #-}
 {-# OPTIONS_GHC -Wall -O2 #-}
 module Main where
 
-import Data.List (find)
-import qualified Data.Vector.Unboxed as V
-import qualified Data.Vector.Unboxed.Mutable as VM
+import qualified Data.Vector.Unboxed as VP
+import qualified Data.Vector.Unboxed.Mutable as V
+import Control.Monad
 
-type Str = String
-
+-----------------------------
+--
+--   Containers stuff: we use vector-based solution
+--   (which is a bad idea, time for Part 2 is 20 sec)
+--
 -- mapping: number -> turn id, the number was mentioned last
 -- invariant: initialized entries are > 0, so initial value
 --            for everything unseen is 0
-type V = V.Vector Int
+type V = V.IOVector Int
+type M = IO
 
-(!) :: V -> Int -> Int
-(!) = V.unsafeIndex -- (V.!) --
-(<~) :: V -> Int -> Int -> V
-(<~) v i x = V.modify (\v' -> VM.write v' i x) v
+(!) :: V -> Int -> M Int
+(!) v i
+  | i >= V.length v = pure 0
+  | otherwise       = V.unsafeRead v i
+
+(<~) :: V -> Int -> Int -> M V
+(<~) v i x = do
+  v' <- if len <= i then g else pure v
+  V.unsafeWrite v' i x
+  pure v'
+  where
+  len = V.length v
+  g = V.grow v $ i - len + 500
+
+-------------------------------------------------
+--
+--  Constants
+--
 
 final :: Int
-final = 30000000 -- 2020
+final = 30_000_000 -- 2020
 
 inp :: [Int]
 inp = [6,3,15,13,1,0] -- [0,3,6] --
 
--- Part 1
+initVectorSize :: Int
+initVectorSize = maximum inp + 1
 
-data S = S {
-  trn  :: Int, -- the turn counter
-  lst  :: Int, -- last number seen
-  lst0 :: Int, -- last turn we saw zero (premature optimization)
-  mem  :: V } --,
-  --trc  :: [Int] }
-  deriving Show
-
-initS :: S
-initS = S l (last inp) last0' v' --(reverse inp)
-  where
-  l = length inp
-  last0' = maybe 0 fst . find ((== 0) .snd) .
-    reverse . zip [1..] $ inp
-  v = V.replicate (final + 1) 0
-  v' = V.update v (V.fromList $ zip inp [1..])
-
-step :: S -> S
-step S{..}
-  | lst == 0 =
-      S {lst  = lst' lst0,
-         lst0 = trn,
-         trn  = trn', ..}
-  | otherwise =
-      S {lst  = lst' (mem ! lst),
-         mem = mem',
-         trn  = trn', ..}
-  where
-  lst' 0 = 0
-  lst' p = trn - p
-  trn' = trn + 1
-  mem' = mem <~ lst $ trn
-
+------------------------------------------------
+--
 -- Part 2
+
+initMem :: M V
+initMem = v'
+  where
+  v = VP.replicate initVectorSize 0
+  v' = VP.thaw $ VP.update v (VP.fromList $ zip inp [1..])
+
+step :: (V, Int) -> Int -> M (V, Int)
+step (mem, lst) trn = do
+    prev <- (mem ! lst)
+    mem' <- mem <~ lst $ trn
+    let next = if prev == 0 then 0 else trn - prev
+    pure $ (mem', next)
 
 -- Main
 
 main :: IO ()
 main = do
-  let res1 = lst $ iterate step initS !! (final - length inp) -- '-' -- foldl' procLine initS $ inp
-  print res1
-  let res2 = '-'
+  mem <- initMem
+  (_mem', res2) <- foldM step (mem, last inp)
+    [length inp .. final - 1]
   print res2
-  print "--"
