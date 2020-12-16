21738ef381dee2a9fad9ed7db2cc35e9ebad4b42
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2020-12-16 10:42:58 -0500

    day-15: experiments to improve perf (failed but looks better now)

--- day-15.hs
+++ day-15.hs
@@ -1,33 +1,69 @@
 #!/usr/bin/env cabal
 {- cabal:
-build-depends: base, vector
+build-depends: base, vector, pretty-simple
 -}
-{-# language TypeApplications, RecordWildCards, StrictData, ViewPatterns #-}
-{-# OPTIONS_GHC -Wall -O2 #-}
+{-# language TypeApplications, RecordWildCards, StrictData, ViewPatterns, BlockArguments, NumericUnderscores #-}
+{-# OPTIONS_GHC -Wall -O2 -rtsopts #-}
 module Main where
 
 import Data.List (find)
 import qualified Data.Vector.Unboxed as V
 import qualified Data.Vector.Unboxed.Mutable as VM
+--import Text.Pretty.Simple
+--import Debug.Trace
+import Control.Monad.ST
 
 type Str = String
 
+
+-----------------------------
+--
+--   Containers stuff
+--
 -- mapping: number -> turn id, the number was mentioned last
 -- invariant: initialized entries are > 0, so initial value
 --            for everything unseen is 0
 type V = V.Vector Int
 
+{-
 (!) :: V -> Int -> Int
 (!) = V.unsafeIndex -- (V.!) --
 (<~) :: V -> Int -> Int -> V
 (<~) v i x = V.modify (\v' -> VM.write v' i x) v
+-}
+
+
+(!) :: V -> Int -> Int
+(!) v i
+  | i >= V.length v = 0
+  | otherwise       = V.unsafeIndex v i -- (V.!) --
+(<~) :: V -> Int -> Int -> V
+(<~) v i x =
+  if len <= i
+    then g
+    else w
+  where
+  len = V.length v
+  g = runST do
+    v' <- V.unsafeThaw v
+    v'' <- VM.grow v' $ i - len + 500
+    VM.write v'' i x
+    V.unsafeFreeze v''
+  w = (\mv -> VM.unsafeWrite mv i x) `V.modify` v
+
+-------------------------------------------------
 
 final :: Int
-final = 30000000 -- 2020
+final = 60000 -- 30_000_000 -- 2020
 
 inp :: [Int]
-inp = [6,3,15,13,1,0] -- [0,3,6] --
+inp =  [6,3,15,13,1,0] --[0,3,6] --
 
+initVectorSize :: Int
+initVectorSize = 200
+
+------------------------------------------------
+--
 -- Part 1
 
 data S = S {
@@ -38,13 +74,13 @@ data S = S {
   --trc  :: [Int] }
   deriving Show
 
-initS :: S
-initS = S l (last inp) last0' v' --(reverse inp)
+initS :: Int -> S
+initS ivs = S l (last inp) last0' v' -- (reverse inp)
   where
   l = length inp
   last0' = maybe 0 fst . find ((== 0) .snd) .
     reverse . zip [1..] $ inp
-  v = V.replicate (final + 1) 0
+  v = V.replicate (ivs + 1) 0
   v' = V.update v (V.fromList $ zip inp [1..])
 
 step :: S -> S
@@ -52,25 +88,38 @@ step S{..}
   | lst == 0 =
       S {lst  = lst' lst0,
          lst0 = trn,
-         trn  = trn', ..}
+         trn  = trn',..}
+         --trc=lst' lst0:trc,..}
   | otherwise =
       S {lst  = lst' (mem ! lst),
          mem = mem',
-         trn  = trn', ..}
+         trn  = trn',..}
+         --trc=lst' lst0:trc, ..}
   where
   lst' 0 = 0
   lst' p = trn - p
   trn' = trn + 1
   mem' = mem <~ lst $ trn
 
--- Part 2
+part1 :: Int -> S
+part1 ivs = iterate step (initS ivs) !! (final - length inp)
 
 -- Main
 
 main :: IO ()
 main = do
-  let res1 = lst $ iterate step initS !! (final - length inp) -- '-' -- foldl' procLine initS $ inp
+  let its = part1 initVectorSize
+      --itf = part1 final
+      res1 = lst its -- (lst it, trc it)
+      {-
+      (s1,s2) = head . dropWhile
+        (\(s1, s2) -> mem s1 == V.take (V.length $ mem s1) (mem s2)
+          && (V.all (==0) $ V.drop (V.length $ mem s1) (mem s2))) $
+        zip its itf
+      -}
   print res1
-  let res2 = '-'
-  print res2
+  -- ~ print "--"
+  -- ~ print $ part1 final
+  --let res2 = '-'
+  --print res2
   print "--"
