6e02650ec79e9844ff862434e70e5b25b8c33a4e
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2021-05-02 21:29:05 -0400

    start day-11, p2: compute one neighbourhood according to the new def

--- day-11.hs
+++ day-11.hs
@@ -2,32 +2,37 @@
 {- cabal:
 build-depends: base, vector, primitive, extra, ansi-terminal
 -}
-{-# language TupleSections, BlockArguments, MultiWayIf, ImplicitParams #-}
+{-# language TupleSections, BlockArguments, MultiWayIf, ImplicitParams,
+             ConstraintKinds #-}
 {-# OPTIONS_GHC -Wall -O1 #-}
 module Main where
 
 import System.Console.ANSI
 import System.Time.Extra
 import Data.Bool
+import Data.Maybe
 import Data.Functor ((<&>))
 import Control.Monad hiding (void)
 import Control.Monad.Primitive
 import qualified Data.Vector.Unboxed as V
+import qualified Data.Vector as VV
 import qualified Data.Vector.Unboxed.Mutable as VM
 --import qualified Data.Vector.Generic as GV
 
 -- container
 -- rows, cols, table as a map Int -> Char
 type C = V.MVector (PrimState IO) Char
+type CPure = V.Vector Char
 type CL = (Int, Int, C)
+type Ctx = (?rw :: Int, ?cl :: Int, ?ns :: Ns)
 
 -----------------   generic aux stuff
 
 (!) :: C -> Int -> IO Char
-(!) = VM.unsafeRead
+(!) = VM.read
 
 (<~) :: C -> Int -> Char -> IO ()
-(<~) = VM.unsafeWrite
+(<~) = VM.write
 
 count :: Eq a => a -> [a] -> Int
 count x = length . filter (== x)
@@ -35,10 +40,10 @@ count x = length . filter (== x)
 -----------------  task-specific aux stuff
 
 -- print the field
-p ::
-  (?rw :: Int, ?cl :: Int) =>
+prt ::
+  Ctx =>
   C -> IO ()
-p v = foldM_ s () is
+prt v = foldM_ s () is
   where
   s _ i = do
     st <- v ! i
@@ -47,9 +52,19 @@ p v = foldM_ s () is
 
 -- 1-dimensional indices for a field of size rw x cl
 is ::
-  (?rw :: Int, ?cl :: Int) =>
+  Ctx =>
   [Int]
 is = [0 .. ?rw * ?cl - 1]
+
+posToAbs ::
+  Ctx =>
+  (Int, Int) -> Int
+posToAbs (ri, ci) = ri * ?cl + ci
+
+absToPos ::
+  Ctx =>
+  Int -> (Int, Int)
+absToPos i = i `divMod` ?cl
 
 free, took, void :: Char
 free = 'L'
@@ -59,15 +74,15 @@ void = '.'
 -------------------- Part 1
 
 animate :: Bool
-animate = True
+animate = False
 
 loop ::
-  (?rw :: Int, ?cl :: Int) =>
+  Ctx =>
   C -> C -> IO ()
 loop ci co = do
   when animate do
     clearScreen
-    p ci
+    prt ci
     sleep 1
   changed <- iter ci co
   if changed
@@ -75,18 +90,17 @@ loop ci co = do
     else pure ()
 
 iter ::
-  (?rw :: Int, ?cl :: Int) =>
+  Ctx =>
   C -> C -> IO Bool
 iter ci co = foldM (upd ci co) False is
 
 upd ::
-  (?rw :: Int, ?cl :: Int) =>
+  Ctx =>
   C -> C -> Bool -> Int -> IO Bool
 upd ci co changed i = do
   c <- ci ! i
   if c == void then pure changed else do
     w <- weight ci i
-    --putStrLn (show w)
     let (c', changed') = if
           | c == free ->
               if w == 0 then (took, True) else (free, False)
@@ -98,7 +112,7 @@ upd ci co changed i = do
 
 -- Number of places taken around
 weight ::
-  (?rw :: Int, ?cl :: Int) =>
+  Ctx =>
   C -> Int -> IO Int
 weight v i = do
       l  <- bool (v ! (i - 1))       mfr isLft
@@ -118,7 +132,53 @@ weight v i = do
   isBot = ri == ?rw - 1
   isLft = ci == 0
   isRgt = ci == ?cl - 1
-  (ri, ci) = i `divMod` ?cl
+  (ri, ci) = absToPos i
+
+-- Part 2: extended n-hood
+
+type N = V.Vector Int
+type P = (Int,Int)
+type Ns = VV.Vector N
+
+weight2 ::
+  Ctx =>
+  C -> Int -> IO Int
+weight2 v i = undefined
+
+nhood ::
+  Ctx =>
+  CPure -> Int -> N
+nhood v i = res
+  where
+  posi = absToPos i
+  ps = paths posi
+  res = V.fromList . catMaybes $ map firstValidM ps
+
+  firstValidM :: [P] -> Maybe Int
+  firstValidM path = let
+    (p:_) = dropWhile (\p' -> isvalid p' && isvoid p') path
+    in
+      if isvalid p
+        then Just (posToAbs p)
+        else Nothing
+
+  isvalid (ri, ci) =
+    0 <= ri && ri < ?rw &&
+      0 <= ci && ci < ?cl
+
+  isvoid p = v V.! (posToAbs p) == void
+
+paths :: P -> [[P]]
+paths (ri, ci) =
+  [
+    [(ri + n*dr, ci + n*dc) | n<-[1..]] |
+    dr<-dirs,
+    dc<-dirs,
+    dr /= 0 || dc /= 0
+  ]
+  where
+  dirs = [-1..1]
+
 
 -- Main
 
@@ -137,7 +197,10 @@ main = do
   (_, _, inp2)   <- getInput
   res1 <-
     let ?rw = rw
-        ?cl = cl in do
+        ?cl = cl
+        ?ns = VV.empty -- FIXME: compute all neighbourhoods
+        -- smth like `VV.fromList $ map (nhood inp1) is`
+    in do
         loop inp1 inp2
         res1' <- foldM
           (\c i -> inp1 ! i <&> \s -> c + bool 0 1 (s==took))
