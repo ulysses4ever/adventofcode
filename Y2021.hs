42bfa953636b15829137a96f106ea277d09d57f0
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2021-12-02 11:17:43 -0500

    day 2 part 2

--- Y2021.hs
+++ Y2021.hs
@@ -6,5 +6,5 @@ import Y2021.Day2
 solutions = [
   undefined, -- all hail 1-based indexing
   (Y2021.Day1.solve1, Y2021.Day1.solve2),
-  (Y2021.Day2.solve1, Y2021.Day2.solve2)
+  (Y2021.Day2.solve 1, Y2021.Day2.solve 2)
   ]
--- Y2021/Day2.hs
+++ Y2021/Day2.hs
@@ -1,19 +1,24 @@
 {-# language BangPatterns #-}
 {-# language LambdaCase   #-}
-module Y2021.Day2 (solve1, solve2) where
+module Y2021.Day2 (solve) where
 
 import Data.List
 
-solve1 :: String -> IO ()
-solve1 = print . compute . parse
+-- first arg is Part
+solve :: Int -> String -> IO ()
+solve n = print . compute n . parse
 
-compute rs = d * w
+compute 1 rs = d * w
   where
   (S d w) = foldl' exec (S 0 0) rs
 
-data Cmd = F | U | D
+compute 2 rs = d * w
+  where
+  (S' a d w) = foldl' exec' (S' 0 0 0) rs
 
+data Cmd = F | U | D
 data St = S !Int !Int
+data St' = S' !Int !Int !Int
 
 exec :: St -> Rec -> St
 exec (S d w) (R c n) = case c of
@@ -21,18 +26,21 @@ exec (S d w) (R c n) = case c of
   U -> S (d-n) w
   D -> S (d+n) w 
 
+exec' :: St' -> Rec -> St'
+exec' (S' a d w) (R c n) = case c of
+  F -> S' a (d+a*n) (w+n)
+  U -> S' (a-n) d w
+  D -> S' (a+n) d w 
+
+data Rec = R Cmd Int
+
 readCmd :: String -> Cmd
 readCmd "forward" = F
 readCmd "up" = U
 readCmd "down" = D
 
-data Rec = R Cmd Int
-
 parse :: String -> [Rec]
 parse = map (readRec . words) . lines
 
 readRec [cmp, arg] = R (readCmd cmp) (read arg) 
-
-solve2 :: String -> IO ()
-solve2 input = print 0
 
