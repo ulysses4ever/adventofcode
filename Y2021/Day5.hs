d8ff265e4042440bc89ffdd78e856120233e40d9
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2021-12-05 15:54:48 -0500

    day-5 part 2

--- Y2021/Day5.hs
+++ Y2021/Day5.hs
@@ -1,12 +1,16 @@
-{-# language BangPatterns #-}
+{-# language BangPatterns  #-}
+{-# language TupleSections #-}
 module Y2021.Day5 (solve) where
 
 import Data.List
+import Data.Function
 import Data.Foldable
 import Data.Traversable
+import Control.Monad
 import Debug.Trace
 
-import Data.List.Extra (split)
+import Data.List.Extra (split, chunksOf)
+import Data.Tuple.Extra (dupe)
 
 import qualified Data.Map.Strict as M
 import Data.Map.Strict (Map)
@@ -21,28 +25,44 @@ type Rec = Line
 type St = Map Pt Int
 
 compute :: Int -> [Rec] -> Int
-compute 1 rs@(r:rs') = res
+compute n rs = res
   where
-  finalState = foldl' updateState M.empty rs
+  finalState = foldl' (updateState n) M.empty rs
   cnt s x = if x > 1 then s + 1 else s
   res = M.foldl' cnt 0 finalState
 
-compute 2 (r:rs) = res
-  where
-  res = 0
-
-updateState :: St -> Line -> St
-updateState m l = {-trace (showM m) $-} foldl' upd m $ fillLine l
+updateState :: Int -> St -> Line -> St
+updateState n m l = -- trace (showM m) $
+  foldl' upd m $ fillLine n l
   where
   upd m p = M.alter ins p m
   ins Nothing = Just 1
   ins (Just n) = Just (n+1)
 
-fillLine :: Line -> [Pt]
-fillLine (p1@(x1,y1), p2@(x2,y2))
-  | x1 == x2 = [(x1,y) | y <- [y1..y2] ++ [y2..y1]]
-  | y1 == y2 = [(x,y1) | x <- [x1..x2] ++ [x2..x1]]
+fillLine :: Int -> Line -> [Pt]
+fillLine n (p1@(x1,y1), p2@(x2,y2))
+  | x1 == x2 = (x1,) `map` fillUp y1 y2
+  | y1 == y2 = (,y1) `map` fillUp x1 x2
+  | n == 2 && (abs (x1 - y1) == abs (x2 - y2) ||
+               abs (x1 - x2) == abs (y1 - y2))
+      = fillDg (sort [p1, p2])
   | otherwise = []
+
+-- assert: x1 < x2
+fillDg :: [Pt] -> [Pt]
+fillDg [(x1,y1), (x2,y2)] = zip [x1..x2] ys
+  where
+  ys = if y1 < y2 then fillUp y1 y2 else fillDw y2 y1
+
+fillUp :: Int -> Int -> [Int]
+fillUp x y
+  | x < y = [x..y]
+  | otherwise = [y..x]
+
+fillDw :: Int -> Int -> [Int]
+fillDw x y
+  | x < y = [y,y-1..x]
+  | otherwise = [x,x-1..y]
 
 parse :: String -> [Rec]
 parse = map readRec . lines
@@ -55,14 +75,13 @@ readRec input = ((x1,y1),(x2,y2))
 
 -- Debug
 showM :: St -> String
-showM m = show m -- "" -- res
+showM m = res
   where
-  ps = M.toList m
+  ps = liftM2 (,) [0..maxCol] [0..maxRow]
   maxCol = 9 -- maximum $ map (fst . fst) ps
   maxRow = 9 -- maximum $ map (snd . fst) ps
-  res =
-    concat $ for [0..maxCol] $ \c ->
-      concat $ (for [0..maxRow] $ \r ->
+  res = unlines $ chunksOf (maxCol + 1) $
+    flip map ps $ \(c,r) ->
         case M.lookup (r,c) m of
-          Nothing -> "."
-          Just n  -> show n) ++ ["\n"]
+          Nothing -> '.'
+          Just n  -> head $ show n
