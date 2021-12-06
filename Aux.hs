30c72b4a12236b832b900c76a2f759613379c49d
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2021-12-05 20:15:27 -0500

    day 5: much simpler line filling
    First, assume we don't have anything except for
    diagonals and vert/horiz line in the input.
    (This is the case).
    
    Second, computing deltas (dx, dy) and using them
    to fill line is much more succinct.

--- Aux.hs
+++ Aux.hs
@@ -4,3 +4,9 @@ import Data.List.Extra (split)
 
 readIntsSep :: Char -> String -> [Int]
 readIntsSep c = map read . split (== c)
+
+sign :: Int -> Int
+sign n
+  | n > 0 = 1
+  | n < 0 = -1
+  | True  = 0
--- Y2021/Day5.hs
+++ Y2021/Day5.hs
@@ -2,6 +2,8 @@
 {-# language TupleSections #-}
 module Y2021.Day5 (solve) where
 
+import Aux
+
 import Data.List
 import Data.Function
 import Data.Foldable
@@ -40,29 +42,15 @@ updateState n m l = -- trace (showM m) $
   ins (Just n) = Just (n+1)
 
 fillLine :: Int -> Line -> [Pt]
-fillLine n (p1@(x1,y1), p2@(x2,y2))
-  | x1 == x2 = (x1,) `map` fillUp y1 y2
-  | y1 == y2 = (,y1) `map` fillUp x1 x2
-  | n == 2 && (abs (x1 - y1) == abs (x2 - y2) ||
-               abs (x1 - x2) == abs (y1 - y2))
-      = fillDg (sort [p1, p2])
-  | otherwise = []
-
--- assert: x1 < x2
-fillDg :: [Pt] -> [Pt]
-fillDg [(x1,y1), (x2,y2)] = zip [x1..x2] ys
-  where
-  ys = if y1 < y2 then fillUp y1 y2 else fillDw y2 y1
-
-fillUp :: Int -> Int -> [Int]
-fillUp x y
-  | x < y = [x..y]
-  | otherwise = [y..x]
-
-fillDw :: Int -> Int -> [Int]
-fillDw x y
-  | x < y = [y,y-1..x]
-  | otherwise = [x,x-1..y]
+fillLine n (p1@(x1,y1), p2@(x2,y2)) = let
+  dx = sign $ x2 - x1
+  dy = sign $ y2 - y1
+  xs = [x1, x1+dx..]
+  ys = [y1, y1+dy..]
+  in
+    if n == 1 && dy /= 0 && dx /= 0
+      then []
+      else p2 : (takeWhile (/= p2) $ zip xs ys)
 
 parse :: String -> [Rec]
 parse = map readRec . lines
@@ -73,7 +61,7 @@ readRec input = ((x1,y1),(x2,y2))
   (p1 : arrow : p2 : _) = words input
   [x1, y1, x2, y2] = map read $ concatMap (split (==',')) [p1,p2]
 
--- Debug
+-- Debug, only for the sample
 showM :: St -> String
 showM m = res
   where
