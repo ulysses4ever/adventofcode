d8141d6644204e3347f12f7b2069b5c9fdbc72cd
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2021-12-09 00:38:57 -0500

    day 8 part 2

--- Y2021/Day8.hs
+++ Y2021/Day8.hs
@@ -3,8 +3,10 @@ module Y2021.Day8 (solve) where
 
 import Aux
 import Data.List
+import Data.Maybe
 import Debug.Trace
 import Control.Arrow (second)
+import Data.List.Extra (groupSortOn)
 
 -- first arg is Part #
 solve :: Int -> String -> IO ()
@@ -14,7 +16,7 @@ data Rec = R {
   left :: [String],
   right :: [String] }
   deriving Show
-data St = S
+type St = [String]
 
 interestingLengths :: [Int]
 --                    1 7 4 8
@@ -29,10 +31,43 @@ compute 1 rs = res
 
 compute 2 rs = res
   where
-  res = 0
+  rd (R l r) = read $ concatMap (show . decode (sortWords l)) r
+  res = sum $ map rd rs
+
+decode :: St -> String -> Int
+decode s w = fromJust $ elemIndex w s
+
+sortWords :: [String] -> [String]
+sortWords ws = res
+  where
+  [[n1], [n7], [n4], l5, l6, [n8]] = groupSortOn length ws
+  [n2, n3, n5] = sort5 l5 n4 n1
+  [n0, n6, n9] = sort6 l6 n4 n1
+  res = [n0, n1, n2, n3, n4, n5, n6, n7, n8, n9]
+
+sort6 :: [String] -> String -> String -> [String]
+sort6 ws@[u, v, w] n4 n1 = [n0, n6, n9]
+  where
+  Just n9 = find (n4 `subset`) ws
+  ws'@[u', v'] = ws \\ [n9]
+  Just n0 = find (n1 `subset`) ws'
+  [n6] = ws' \\ [n0]
+
+sort6 _ _ _ = error "not implemented"
+
+sort5 :: [String] -> String -> String -> [String]
+sort5 ws@[u, v, w] n4 n1  = [n2, n3, n5]
+  where
+  Just n3 = find (n1 `subset`) ws
+  ws'@[u', v'] = ws \\ [n3]
+  (n2, n5) = if length (n4 \\ u') == 2 then (u', v') else (v', u')
+
+sort5 _ _ _ = error "not implemented"
+
+subset s1 s2 = (== length s1) . length $ s1 `intersect` s2
 
 parse :: String -> [Rec]
 parse = map readRec . lines
 
 readRec :: String -> Rec
-readRec = uncurry R . second tail . break (== "|") . words
+readRec = uncurry R . second tail . break (== "|") . map sort . words
