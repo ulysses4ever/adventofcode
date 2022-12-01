dde18c36e3cae6784504230b5f6db49d96303a46
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2022-12-01 17:23:55 +0100

    start Y2022 (afresh)

---
+++ Y2022/day-1.hs
@@ -0,0 +1,21 @@
+#!/usr/bin/env cabal
+{- cabal:
+build-depends: base, extra, flow
+-}
+
+import Flow
+import Data.List.Extra (split)
+import Data.List
+
+main  = print . solve =<< getContents
+
+parse :: String -> [[Int]]
+parse =  lines .> split (== "") .> map (map read)
+
+part :: Int -> [[Int]] -> Int
+part 1 = map sum .> maximum
+part 2 = map sum .> sortOn negate .> take 3 .> sum
+
+solve :: String -> [Int]
+solve input = (part <$> [1,2]) <*> (pure $ parse input)
+
