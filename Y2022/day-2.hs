d1e22ecacb686dff9783351c6ce4e5035acc00ba
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2022-12-02 12:30:09 -0500

    y2022/d2/p2 (I wish I had time to use souffle-haskell)

--- Y2022/day-2.hs
+++ Y2022/day-2.hs
@@ -10,15 +10,14 @@ import Data.List
 
 main  = getContents >>= solve .> print
 
---solve :: String -> [Int]
-solve input = (part <$> [1]) <*> (pure $ parse input)
+solve :: String -> [Int]
+solve input = (part <$> [1,2]) <*> (pure $ parse input)
 
 parse :: String -> [[String]]
 parse =  lines .> map words
 
---part :: Int -> [[Int]] -> Int
-part 1 = map score .> sum
--- part 2 = id
+part :: Int -> [[String]] -> Int
+part n = map (score n) .> sum
 
 data Out  = W | L | D -- win/loose/draw
 data Move = R | P | S -- rock/paper/scissors
@@ -32,14 +31,29 @@ decode = \case
   "Y" -> P
   "Z" -> S
 
+decode2 = \case
+  "X" -> L
+  "Y" -> D
+  "Z" -> W
+
 getOut R P = W
 getOut P S = W
 getOut S R = W
 getOut m1 m2 | m1 == m2 = D
 getOut _ _ = L
 
-score [m1, m2] = scoreOut (getOut (decode m1) (decode m2)) +
+getMove R W = P
+getMove P W = S
+getMove S W = R
+getMove m1 D = m1
+getMove R L = S
+getMove P L = R
+getMove S L = P
+
+score 1 [m1, m2] = scoreOut (getOut (decode m1) (decode m2)) +
   scoreMove (decode m2)
+score 2 [m1, m2] = scoreMove (getMove (decode m1) (decode2 m2)) +
+  scoreOut (decode2 m2)
 
 scoreMove = \case
   R -> 1
