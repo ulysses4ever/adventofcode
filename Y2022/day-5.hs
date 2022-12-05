7115aa17f577c4764f14bd66dde8f25145e99186
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2022-12-05 11:48:09 -0500

    y2022/d5/p1 start

--- Y2022/day-5.hs
+++ Y2022/day-5.hs
@@ -4,12 +4,26 @@ build-depends: base, flow, extra
 -}
 {-# language LambdaCase #-}
 
-import Flow ((.>))
+import Flow ((.>), (|>))
 import Data.List
+import Data.List.Extra (replace)
+import Data.Char
 
 -- Solve part n (n is 1 or 2) of the problem: turn structured input into the result
 -- part :: Int -> ??? -> Int
 part n = id -- map (score n) .> sum
+
+-- Turn problem's full text into something more structured
+-- parse :: String -> ???
+parse i = state3, moves2
+  where
+    (state1, empty:moves1) = i |> lines |> span (/= "")
+    state2 = state1 |> map (replace "   " "[x]") |> map (filter isAlpha)
+    state3 = state2 |> transpose |> map (filter (/= 'x'))
+
+    moves2 :: [[Int]]
+    moves2 = moves1 |> groupBy ((==) `on` isDigit) |> filter (isDigit . head)
+        |> map read
 
 -- Read one line of problem's input into something more structured
 -- parseLine :: String -> ???
@@ -27,9 +41,5 @@ main  = getContents >>= solve .> print
 
 -- Solve both parts and return a list with two elements -- the results
 -- Input: problem's full text
-solve :: String -> [Int]
+--solve :: String -> [Int]
 solve input = (part <$> [1,2]) <*> (pure $ parse input)
-
--- Turn problem's full text into something more structured
--- parse :: String -> ???
-parse =  lines .> map parseLine
