daa964f53774ab17581ef37f5946ee30788f2b81
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2022-12-05 21:37:38 -0500

    y2022/d5/p1

--- Y2022/day-5.hs
+++ Y2022/day-5.hs
@@ -1,33 +1,43 @@
 #!/usr/bin/env cabal
 {- cabal:
-build-depends: base, flow, extra
+build-depends: base, flow, extra, ilist
 -}
 {-# language LambdaCase #-}
 
 import Flow ((.>), (|>))
+import Data.Function
 import Data.List
 import Data.List.Extra (replace)
+import Data.List.Index (setAt)
 import Data.Char
+import Debug.Trace
 
 -- Solve part n (n is 1 or 2) of the problem: turn structured input into the result
 -- part :: Int -> ??? -> Int
-part n = id -- map (score n) .> sum
+part n (state, moves) =
+  map head $ foldl' makeMove state moves
+
+makeMove s [n, from, to] =
+  setAt to' t' (setAt from' f' s)
+  where
+    from' = from - 1
+    to' = to - 1
+    f = s !! from'
+    t = s !! to'
+    (m, f') = splitAt n f
+    t' = reverse m ++ t
 
 -- Turn problem's full text into something more structured
--- parse :: String -> ???
-parse i = state3, moves2
+parse i = (state3, moves2)
   where
     (state1, empty:moves1) = i |> lines |> span (/= "")
-    state2 = state1 |> map (replace "   " "[x]") |> map (filter isAlpha)
+    state2 = state1 |> map (replace "    " " [x]") |> map (filter isAlpha)
     state3 = state2 |> transpose |> map (filter (/= 'x'))
 
-    moves2 :: [[Int]]
-    moves2 = moves1 |> groupBy ((==) `on` isDigit) |> filter (isDigit . head)
-        |> map read
-
--- Read one line of problem's input into something more structured
--- parseLine :: String -> ???
-parseLine = id
+    moves2 = moves1 |> map
+        (groupBy ((==) `on` isDigit)
+         .> filter (isDigit . head)
+         .> map read)
 
 {--------------------------------------------------------
 --
@@ -42,4 +52,4 @@ main  = getContents >>= solve .> print
 -- Solve both parts and return a list with two elements -- the results
 -- Input: problem's full text
 --solve :: String -> [Int]
-solve input = (part <$> [1,2]) <*> (pure $ parse input)
+solve input = (part <$> [1]) <*> (pure $ parse input)
