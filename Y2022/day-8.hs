83e186c03744197503633301ecc8e4f5bea38761
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2022-12-08 23:16:10 +0100

    y2022/d8/p1

--- Y2022/day-8.hs
+++ Y2022/day-8.hs
@@ -4,20 +4,29 @@ build-depends: base, flow, extra
 -}
 {-# language LambdaCase #-}
 
-import Flow ((.>))
+import Flow ((.>), (|>))
 import Data.List
-import Data.Word
+import Data.List.Extra
+import Data.Int
+import Debug.Trace
 
 -- Solve part n (n is 1 or 2) of the problem: turn structured input into the result
 -- part :: Int -> ??? -> Int
-part n i = sum (map pp i) + sum (map pp (transpose i))
+part n i = length .
+  sortOn fst . nubOrd $ (concatMap pp j) ++ (concatMap pp (transpose j))
+  where
+  w = length $ head i
+  j = concat i |> zip [1..] |> chunksOf w
 
-p l = 1 + (length . takeWhile (\(a,b) -> a < b) $ zip l (tail l))
+p = snd . foldl'
+  (\ s@(mx, ps) p@(_, a) -> traceShow (s,p) $
+    if mx < a then (a, p:ps) else s)
+  (-1, [])
 
-pp l = p l + p (reverse l)
+pp l = p l ++ p (reverse l)
 
 -- Read one line of problem's input into something more structured
-parseLine :: String -> [Word8]
+parseLine :: String -> [Int8]
 parseLine = map (pure .> read)
 
 {--------------------------------------------------------
@@ -32,7 +41,7 @@ main  = interact (solve .> show)
 
 -- Solve both parts and return a list with two elements -- the results
 -- Input: problem's full text
-solve input = (part <$> [1,2]) <*> (pure $ parse input)
+solve input = (part <$> [1]) <*> (pure $ parse input)
 
 -- Turn problem's full text into something more structured
 -- parse :: String -> ???
