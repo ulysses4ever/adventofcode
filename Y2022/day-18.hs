8be0adb07582b5394ee2024b8a291ad44b2558f1
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2022-12-18 23:31:00 -0500

    y2022/d18/p2 not working yet (doesn't account for larger holes)

--- Y2022/day-18.hs
+++ Y2022/day-18.hs
@@ -3,6 +3,7 @@
 build-depends: base, flow, extra, linear, containers
 -}
 {-# language LambdaCase #-}
+{-# language TupleSections #-}
 
 import Flow ((.>), (|>))
 import Data.List
@@ -12,20 +13,39 @@ import qualified Data.Set as S
 import Data.Set (Set)
 import Linear.Vector
 import Linear.V3
+import Debug.Trace
 
 type V = V3 Int
 
 -- Solve part n (n is 1 or 2) of the problem: turn structured input into the result
 -- part :: Int -> [V] -> Int
-part n inp = res
+part n inp = res n
   where
     ps = S.fromList inp
-    res = foldl' f (S.size ps * 6)  ps
-    f n p
-      =  map (p ^+^) basis ++ map (p ^-^) basis
+    res 1 = foldl' f (S.size ps * 6)  ps
+    res 2 = res 1 - 6 * countTrapped ps
+    f n p =  countNeighboursInSet p ps |> (n -)
+
+countTrapped ps = S.size . snd . foldl' g (S.empty, S.empty) $ ps
+  where
+    g state = neighbours
+      .> foldl' (notVisitedOrTrapped ps) state
+
+countNeighboursInSet p ps
+      =  neighbours p
       |> filter (`S.member` ps)
       |> length
-      |> (n -)
+
+notVisitedOrTrapped ps state@(visited, trapped) n
+  | n `S.member` visited = (visited, trapped)
+  | countNeighboursInSet n ps == 6
+    && n `S.notMember` ps = traceShow n $ visited' trapped'
+  | otherwise = visited' trapped
+    where
+      visited' = (S.insert n visited,)
+      trapped' = S.insert n trapped
+
+neighbours p = map (p ^+^) basis ++ map (p ^-^) basis
 
 -- Read one line of problem's input into something more structured
 parseLine :: String -> V
