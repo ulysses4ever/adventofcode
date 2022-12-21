f72de8ae17fbc9238e7121f8ebb50c5b8518b80d
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2022-12-20 23:01:06 -0500

    y2022/d20/p1 start

--- Y2022/day-20.hs
+++ Y2022/day-20.hs
@@ -1,16 +1,35 @@
 #!/usr/bin/env cabal
 {- cabal:
-build-depends: base, flow, extra
+build-depends: base, flow, extra, vector
 -}
 {-# language LambdaCase #-}
 
 import Flow ((.>), (|>))
+import Control.Monad.ST (runST)
 import Data.List
 import Data.Maybe
+import qualified Data.Vector.Unboxed.Mutable as V
+import Data.Vector.Unboxed (Vector, fromList, unsafeThaw, unsafeFreeze)
+import qualified Data.Vector.Unboxed as VI (length)
+
+type V = Vector Int
 
 -- Solve part n (n is 1 or 2) of the problem: turn structured input into the result
 -- part :: Int -> ??? -> Int
-part n = length
+part n inp = length inp
+  where
+    v = fromList inp
+
+mix v = runST (unsafeThaw v >>= go 0 >>= unsafeFreeze)
+  where
+    go len mv = pure mv
+    go i mv = do
+      x <- V.unsafeRead i mv
+      -- let i' = if x > 0 then
+      let j = (i + x + len) `mod` len
+          -- s1 = V.slice (min i j) (abs (i - j + 1) mv
+      pure mv
+    len = VI.length v
 
 -- Read one line of problem's input into something more structured
 parseLine :: String -> Int
