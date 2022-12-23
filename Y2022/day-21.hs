f5a34958751082ec89ae2bce7aaa22be14b0745d
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2022-12-22 19:49:05 -0500

    y2022/d21/p2

--- Y2022/day-21.hs
+++ Y2022/day-21.hs
@@ -3,6 +3,7 @@
 build-depends: base, flow, extra, containers
 -}
 {-# language LambdaCase #-}
+{-# language TupleSections #-}
 
 import Flow ((.>), (|>))
 import Data.List
@@ -11,21 +12,62 @@ import qualified Data.Map as M
 
 -- Solve part n (n is 1 or 2) of the problem: turn structured input into the result
 -- part :: Int -> ??? -> Int
-part n inp = prog M.! "root"
+part n inp = res n
   where
+    res 1 = prog M.! "root"
+    res 2 = go root2humn (prog M.! first)
+      where
+        [opd1, op, opd2] = vars M.! "root"
+        first | head root2humn == opd1 = opd2
+              | otherwise = opd1
+        go ["humn"] val = val
+        go (v:v':vs) val = go (v':vs) val'
+          where
+            [opd1, op, opd2] = vars M.! v
+            (knw, dir)
+              | opd1 == v' = (opd2, Left ())
+              | opd2 == v' = (opd1, Right ())
+              | otherwise = error ("Unknown operand: " ++ v)
+            val' = inv op dir val (prog M.! knw)
+
+    -- part 1
     prog = M.fromList $ map interp inp
     interp = \case
-      [v, n] -> (init v, read n :: Int)
-      [v, opd1, op, opd2] -> (init v, (parseOp op) (prog M.! opd1) (prog M.! opd2))
-    parseOp = \case
-      "+" -> (+)
-      "-" -> (-)
-      "*" -> (*)
-      "/" -> (div)
+      (v, [n]) -> (v, read n :: Int)
+      (v, [opd1, op, opd2]) -> (v, (parseOp op) (prog M.! opd1) (prog M.! opd2))
+
+    -- part 2
+    vars = M.fromList inp
+    refs = M.fromList . concat . catMaybes $ unfoldr (\case
+        [] -> Nothing
+        (v, assoc) : rest -> Just . (,rest) $ case assoc of
+          [n] -> Nothing
+          [opd1, op, opd2] -> Just [(opd1, v), (opd2, v)]
+        ) inp
+    root2humn = go "humn" []
+      where
+        go r@"root" path = path
+        go v path = go (refs M.! v) (v:path)
+
+inv op dir val knownOpd
+  | op == "+" || op == "*" || dir == Left () = invOp op val knownOpd
+  | dir == Right () = parseOp op knownOpd val
+  where
+    invOp = \case
+      "+" -> (-)
+      "-" -> (+)
+      "*" -> div
+      "/" -> (*)
+
+parseOp = \case
+  "+" -> (+)
+  "-" -> (-)
+  "*" -> (*)
+  "/" -> div
 
 -- Read one line of problem's input into something more structured
 -- parseLine :: String -> ???
-parseLine = words
+parseLine = words .> \case v:rest -> (init v, rest)
 
 {--------------------------------------------------------
 --
