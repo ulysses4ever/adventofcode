8f03429dfd2ef535878b9c05fcf9709906b78981
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2022-12-31 12:34:55 -0500

    y2022/d20/p1 (finally!)

--- Y2022/day-20.hs
+++ Y2022/day-20.hs
@@ -1,6 +1,6 @@
 #!/usr/bin/env cabal
 {- cabal:
-build-depends: base, flow, extra, vector, containers
+build-depends: base, flow, extra, vector
 -}
 {-# language LambdaCase #-}
 
@@ -10,36 +10,46 @@ import Data.List
 import Data.Maybe
 import qualified Data.Vector.Unboxed.Mutable as V
 import Data.Vector.Unboxed (Vector, fromList, unsafeThaw, unsafeFreeze)
-import qualified Data.Vector.Unboxed as VI (length)
+import qualified Data.Vector.Unboxed as VI
 import Debug.Trace
 
-type V = Vector Int
+-- element + flag if it has to be moved
+type V = Vector (Int, Bool)
 
 -- Solve part n (n is 1 or 2) of the problem: turn structured input into the result
 -- part :: Int -> ??? -> Int
-part n inp = seq (mix v) 1
+part n inp = res n
   where
-    v = fromList inp
+    res 1 = z 1000 + z 2000 + z 3000
+    res 2 = 0
 
--- v=fromList [1,2,3::Int]
+    v = fromList $ zip inp $ repeat True -- at start, everyone needs a move
+    v' = VI.map fst $ mix v
+    zeroIdx = VI.elemIndex 0 v' |> fromJust
+    a <+> b = (a + b) `mod` (VI.length v')
+    z n = v' VI.! (zeroIdx <+> n)
 
 mix v = runST (unsafeThaw v >>= go 0 >>= unsafeFreeze)
   where
     go i mv
-      | i == len = traceShow v (pure mv)
+      | i == len = pure mv -- traceShow (VI.map fst v) (pure mv)
       | otherwise = do
-        x <- V.unsafeRead mv i
-        let j = (i + x + len) `mod` len
+        (x, inQueue) <- V.unsafeRead mv i
+        let j = (i + x + (x + i) `div` len) `mod` len
             width = abs (i - j)
             (startSrc, startTrg)
               | i <= j = (i+1, i)
               | otherwise = (j,j+1)
             src = V.slice startSrc width mv
             trg = V.slice startTrg width mv
-        traceShow v (pure ())
-        V.move trg src
-        V.unsafeWrite mv j x
-        go (if j < i then i else i+1) mv
+        if inQueue
+          then do
+            -- traceShow (VI.map fst v) (pure ())
+            V.move trg src
+            V.unsafeWrite mv j (x, False)
+            go (if j < i then i+1 else i) mv
+          else
+            go (i+1) mv
     len = VI.length v
 
 -- Read one line of problem's input into something more structured
