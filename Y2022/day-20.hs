200e351abc1183f89a29c229e4aa6073dddd5739
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2023-01-03 12:04:11 -0500

    y2022/d20/p2 finally... the formula was much simpler all along

--- Y2022/day-20.hs
+++ Y2022/day-20.hs
@@ -8,26 +8,29 @@ import Flow ((.>), (|>))
 import Control.Monad.ST (runST)
 import Data.List
 import Data.Maybe
-import qualified Data.Vector.Unboxed.Mutable as V
-import Data.Vector.Unboxed (Vector, fromList, unsafeThaw, unsafeFreeze)
-import qualified Data.Vector.Unboxed as VI
+import qualified Data.Vector.Mutable as V
+import Data.Vector (Vector, fromList, unsafeThaw, unsafeFreeze)
+import qualified Data.Vector as VI
 import Debug.Trace
 
 -- element + flag if it has to be moved
-type V = Vector (Int, Bool)
 
 -- Solve part n (n is 1 or 2) of the problem: turn structured input into the result
--- part :: Int -> ??? -> Int
-part n inp = res n
+part n inp = -- traceShow v'' $
+  res n
   where
     res 1 = score v'
     res 2 = score v''
 
     score v = sum $ map (z v) [1000, 2000, 3000]
+    key = 811589153 :: Int -- eger
 
-    v = fromList $ zip [0..] inp
     v' = VI.map snd $ mix v
-    v'' = VI.map snd $ iterate mix v !! 1
+      where
+        v = fromList $ zip [0..] inp
+    v'' = VI.map snd $ iterate mix v !! 10
+      where
+        v = fromList $ zip [0..] (map ((* key)) inp)
 
     zeroIdx v = VI.elemIndex 0 v |> fromJust
     z v n = v VI.! ((zeroIdx v + n) `mod` (VI.length v))
@@ -37,12 +40,10 @@ mix v = runST (unsafeThaw v' >>= go 0 >>= unsafeFreeze) |> VI.map fst
     v' = VI.zip v $ VI.replicate len True -- at start, everyone needs a move
     go i' mv
       | i' == len = pure mv
-        -- traceShow (VI.map (fst .> snd) v') (pure mv)
       | otherwise = do
-        -- trace ("Step " ++ show i') (pure())
         let Just i = VI.findIndex (fst .> fst .> (== i')) v'
         ((_, x), inQueue) <- V.unsafeRead mv i
-        let j = (i + x + (x + i) `div` len) `mod` len
+        let j = (i + x) `mod` (len - 1) -- (i + x + (x + i) `div` len) `mod` len
             width = abs (i - j)
             (startSrc, startTrg)
               | i <= j = (i+1, i)
@@ -80,5 +81,5 @@ main  = interact (solve .> show)
 solve input = (part <$> [1,2]) <*> pure (parse input)
 
 -- Turn problem's full text into something more structured
--- parse :: String -> ???
+parse :: String -> [Int]
 parse =  lines .> map parseLine
