c9eaea61978670d56b300da9aa09766bb2da13c8
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2023-01-13 23:09:16 -0500

    y2022/d12/p1

--- Y2022/day-12.hs
+++ Y2022/day-12.hs
@@ -1,15 +1,60 @@
 #!/usr/bin/env cabal
 {- cabal:
-build-depends: base, flow, extra
+build-depends: base, flow, extra, search-algorithms, linear
 -}
 {-# language LambdaCase #-}
+{-# language BlockArguments #-}
+{-# language PatternSynonyms #-}
 
 import Flow ((.>), (|>))
 import Data.List
+import Algorithm.Search
+import Linear.Vector
+import Linear.V2
+import Data.Maybe
+import Data.Char
+import Debug.Trace
+
+type P = V2 Int
+pattern P x y = V2 x y
+
+type El = Char
+type C = [[El]]
 
 -- Solve part n (n is 1 or 2) of the problem: turn structured input into the result
 -- part :: Int -> ??? -> Int
-part n = id -- map (score n) .> sum
+part p inp = res
+  where
+    res = bfs
+      next
+      (\p -> get inp p == 'E')
+      (initial inp)
+      |> fromJust
+      |> length
+    next p = neighbors p
+      |> filter inBounds
+      |> filter (elevateOk p)
+    elevateOk p p' = elev p' <= elev p + 1
+    elev p = ord case get inp p of
+          'S' -> 'a'
+          'E' -> 'z'
+          e   -> e
+    (P rmx cmx) = size2 inp
+    inBounds (P x y) = 0 <= x && x < rmx &&
+      0 <= y && y < cmx
+
+get :: C -> P -> El
+get cont (P r c) = cont !! r !! c
+
+initial :: C -> P
+initial inp = P (length pre) (findIndex (== 'S') tar |> fromJust)
+  where
+    (pre, tar:post) = span ('S' `notElem`) inp
+
+size2 :: [[a]] -> P
+size2 m = P (length m) (length $ head m)
+
+neighbors p = map (p +) basis ++ map (p -) basis
 
 -- Read one line of problem's input into something more structured
 -- parseLine :: String -> ???
