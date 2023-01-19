5f115e3cd47034dc1da1b1b2a2af9e8ab6ddddbe
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2023-01-18 22:30:56 -0500

    y2022/d14/p1 slow progress through dfs

--- Y2022/day-14.hs
+++ Y2022/day-14.hs
@@ -13,25 +13,62 @@ import Data.Foldable.Extra (notNull)
 import Control.Arrow ((***))
 
 import qualified Data.HashMap.Strict as M
+import qualified Data.HashSet as S
 
 import Debug.Trace
 
 import Linear.Vector
 import Linear.V2
+
 type P = V2 Int
 pattern P x y = V2 x y
 
--- Solve part n (n is 1 or 2) of the problem: turn structured input into the result
--- part :: Int -> ??? -> Int
-part n inp = ms
-  where
-    ms@(lx, ly) = concat inp
-      |> partitionEithers
-      |> (make_maps *** make_maps)
-    make_maps = M.fromListWith (++)
-
 type Seg = (Int,Int)
 type KSegs = (Int, [Seg])
+type M = M.HashMap Int [Seg]
+type Rocks = (M,M)
+
+-- Solve part n (n is 1 or 2) of the problem: turn structured input into the result
+-- part :: Int -> ??? -> Int
+part n inp = countSand rs
+  where
+    rs = structureRocks inp
+
+type Set = S.HashSet P
+type State = (Set, Set, Bool)
+
+countSand :: Rocks -> Int
+countSand rs = S.size closed
+  where
+    initial = P 500 0
+    (_,closed,_) = go (S.singleton initial, S.empty, False) initial
+
+    isHalting = undefined -- TODO
+    go :: State -> P -> State
+    go s@(discovered, closed, halt) current
+      | halt = s
+      | isHalting current = (discovered, closed, True)
+      | otherwise = (discovered'', current `S.insert` closed', halt')
+          where
+            ns = neighbors current
+              |> filter ((`S.member` discovered) .> not)
+            discovered' = discovered `S.union` (S.fromList ns)
+            (discovered'', closed', halt') = foldl' go (discovered', closed, halt) ns
+
+neighbors p = undefined -- TODO
+
+structureRocks inp = concat inp
+    |> partitionEithers
+    |> (make_maps *** make_maps)
+  where
+    make_maps = M.fromListWith (++)
+
+collide :: Rocks -> P -> Bool
+collide (mx,my) (P x y) = lkp mx x y || lkp my y x
+  where
+    lkp m a b = case m M.!? a of
+      Just segs -> any (\(l,r) -> l <= b && b <= r) segs
+      Nothing   -> False
 
 -- Read one line of problem's input into something more structured
 parseLine :: String -> [Either KSegs KSegs]
