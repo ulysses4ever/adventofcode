3d6df0961700e767789d8e4a7656e5b2d6ff923c
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2023-01-20 14:40:15 -0500

    y2022/d14/p1 something that looks right but doesn't work

--- Y2022/day-14.hs
+++ Y2022/day-14.hs
@@ -1,6 +1,6 @@
 #!/usr/bin/env cabal
 {- cabal:
-build-depends: base, flow, extra, linear, unordered-containers
+build-depends: base, flow, extra, linear, unordered-containers, lens
 -}
 {-# language LambdaCase #-}
 {-# language PatternSynonyms #-}
@@ -10,7 +10,7 @@ import Data.List
 import Data.Either
 import Data.List.Extra (chunksOf, split)
 import Data.Foldable.Extra (notNull)
-import Control.Arrow ((***))
+import Control.Arrow ((***), (&&&))
 
 import qualified Data.HashMap.Strict as M
 import qualified Data.HashSet as S
@@ -19,6 +19,8 @@ import Debug.Trace
 
 import Linear.Vector
 import Linear.V2
+
+import Control.Lens.Operators ((^.))
 
 type P = V2 Int
 pattern P x y = V2 x y
@@ -30,32 +32,43 @@ type Rocks = (M,M)
 
 -- Solve part n (n is 1 or 2) of the problem: turn structured input into the result
 -- part :: Int -> ??? -> Int
-part n inp = countSand rs
-  where
-    rs = structureRocks inp
+part n = countSand
 
 type Set = S.HashSet P
 type State = (Set, Set, Bool)
 
-countSand :: Rocks -> Int
-countSand rs = S.size closed
+-- countSand :: Rocks -> Int
+countSand inp = trace (showPoints $ S.toList closed) -- S.size
+  closed
   where
     initial = P 500 0
-    (_,closed,_) = go (S.singleton initial, S.empty, False) initial
+    rs = structureRocks inp
+    maxy = foldl' mxFold 0 $ concat inp
+      where
+        mxFold mx (Left (x, ys)) = max mx (maximum $ map snd ys)
+        mxFold mx (Right (y, _)) = max y mx
 
-    isHalting = undefined -- TODO
+    (_,closed,_mx ) = go (S.singleton initial, S.empty, False) initial
+
+    isHalting (P _ y) = y == maxy
     go :: State -> P -> State
     go s@(discovered, closed, halt) current
+      | traceShow current False = undefined
       | halt = s
       | isHalting current = (discovered, closed, True)
       | otherwise = (discovered'', current `S.insert` closed', halt')
           where
             ns = neighbors current
               |> filter ((`S.member` discovered) .> not)
+              |> filter (not . collide rs)
             discovered' = discovered `S.union` (S.fromList ns)
             (discovered'', closed', halt') = foldl' go (discovered', closed, halt) ns
 
-neighbors p = undefined -- TODO
+neighbors p = (p +) <$>
+  [ (P    0 1)
+  , (P (-1) 1)
+  , (P    1 1)
+  ]
 
 structureRocks inp = concat inp
     |> partitionEithers
@@ -87,6 +100,31 @@ parseLine = split (`elem` ", ->")
         x2 = read sx2
         y1 = read sy1
         y2 = read sy2
+
+
+{--------------------------------------------------------
+--
+-- Debug print
+--
+--------------------------------------------------------}
+minMax2D ps = ((xmin, xmax), (ymin, ymax))
+  where
+    mnmx ps lens = ps
+      |> map (\v -> v ^. lens)
+      |> (minimum &&& maximum)
+    mnmxVal@[(xmin, xmax), (ymin, ymax)] = map (mnmx ps) [_x, _y]
+
+showPoints :: [P] -> String
+showPoints ps = unlines $ foldl'
+    (\ls r ->
+      ls ++ (pure $ foldl' (\cs c ->
+        cs ++ pure (if V2 r c `S.member` psSet then '#' else '.'))
+      [] [ymin..ymax]))
+    [] [xmin..xmax]
+  where
+    ((xmin, xmax), (ymin, ymax)) = minMax2D ps
+    psSet = S.fromList ps
+
 
 {--------------------------------------------------------
 --
