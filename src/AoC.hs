23ff2d1924a0f50713ceed33c7832305c385284e
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2023-02-27 22:12:07 -0500

    Input type class and first instances

--- src/AoC.hs
+++ src/AoC.hs
@@ -1,4 +1,29 @@
-module AoC (someFunc) where
+{-# language TypeSynonymInstances #-}
+{-# language DerivingStrategies   #-}
+{-# language GeneralizedNewtypeDeriving   #-}
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+module AoC where
+
+import Data.Aeson
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BSC
+import Flow ((.>)) --, (|>))
+import Data.List.Extra (split)
+import Data.Maybe (fromJust)
+
+class Input a where
+  input :: String -> a
+
+type Str = BS.ByteString
+
+instance Input Str where
+  input = BSC.pack
+
+newtype LineGroups a = LineGroups [[a]]
+  deriving newtype Show
+
+instance Input a => Input (LineGroups a) where
+  input = lines .> split (== "") .> map (map input) .> LineGroups
+
+instance Input Value where
+  input = BSC.pack .> BSC.fromStrict .> decode .> fromJust
