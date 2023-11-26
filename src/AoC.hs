061b509ec636443f17bdd56417b7da20265fdf9c
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2023-11-25 20:43:51 -0500

    add test suite, parse groups, test that it works on Y2022/d1

--- src/AoC.hs
+++ src/AoC.hs
@@ -8,7 +8,8 @@ module AoC (
   (.>), (|>),
   P, P3,
   Input(input),
-  trace, traceShow
+  HomoGroups,
+  trace, traceShow, pPrint
   )
   where
 
@@ -17,13 +18,14 @@ import Data.Function (on)
 import Data.Maybe (fromJust)
 import Data.List (groupBy)
 import Debug.Trace
-
+import Data.Coerce
 
 import Data.Aeson
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as BSC
 import Flow ((.>), (|>))
 import Data.List.Extra (split, splitOn)
+import Text.Pretty.Simple (pPrint)
 
 import Linear.V2
 import Linear.V3
@@ -42,8 +44,11 @@ type Str = BS.ByteString
 instance Input Str where
   input = BSC.pack
 
-groups :: [Char] -> [[Char]]
+groups :: String -> [String]
 groups = splitOn "\n\n"
+
+newtype HomoGroups a = HG [a]
+  deriving Show
 
 instance (Input a, Input b) => Input (a, b) where
   input = groups .> \case [a, b] -> (input a, input b)
@@ -51,7 +56,7 @@ instance (Input a, Input b) => Input (a, b) where
 instance (Input a, Input b, Input c) => Input (a, b, c) where
   input = groups .> \case [a, b, c] -> (input a, input b, input c)
 
-instance Input Value where
+instance Input Value where -- AoC tasks often use JSON as an input
   input = BSC.pack .> BSC.fromStrict .> decode .> fromJust
 
 instance Input Int where
@@ -60,7 +65,7 @@ instance Input Int where
 instance Input P where -- "x,y"
   input = split (== ',') .> \case [x,y] -> V2 (read x) (read y)
 
-instance Input P3 where -- "x,y"
+instance Input P3 where -- "x,y,z"
   input = split (== ',') .> \case [x,y,z] -> V3 (read x) (read y) (read z)
 
 instance {-# OVERLAPPING  #-} Input [Int] where
@@ -71,3 +76,6 @@ instance {-# OVERLAPPING  #-} Input [Int] where
 
 instance {-# OVERLAPPABLE #-} Input a => Input [a] where
   input = lines .> map input
+
+instance Input a => Input (HomoGroups a) where
+  input = groups .> map input .> HG
