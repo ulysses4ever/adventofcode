ddfe74223ea7a75efe898bbc42939cbb8c438422
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2023-11-27 11:46:23 -0500

    get-todays-input.sh: parametrize output destination

--- get-todays-input.sh
+++ get-todays-input.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
 #
 # Get AoC input.
-# Parameters (passed via env vars):
+# Parameters, passed via env vars, all optional:
 # - SESSION -- filename with a cookie to access the AoC website.
 #              Default: ./session
 # - YEAR -- year of the challenge. Default: current year.
 # - DAY -- day of the challenge. Default: current day.
+# - OUT -- where to route the output. Default: $YEAR/input/day-$DAY.txt.
+#          May be useful to set it to '/deb/stdout'.
 #
 set -e
 
 : "${SESSION:=./session}"
 : "${YEAR:=$(date +%Y)}"
 : "${DAY:=$(date +%-d)}"
+: "${OUT:=$YEAR/input/day-$DAY.txt}"
 SESSIONKEY=$(cat "$SESSION")
 # echo "DAY=$DAY, YEAR=$YEAR"
-curl --cookie "session=$SESSIONKEY" https://adventofcode.com/"$YEAR"/day/"$DAY"/input
+curl --cookie "session=$SESSIONKEY" https://adventofcode.com/"$YEAR"/day/"$DAY"/input > "$OUT"
