b54597931eaac09ae5cc66991c329d00b41c700f
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2023-11-28 21:07:14 -0500

    ./get-todays-input.sh: support fetching sample inputs

--- get-todays-input.sh
+++ get-todays-input.sh
@@ -8,13 +8,25 @@
 # - DAY -- day of the challenge. Default: current day.
 # - OUT -- where to route the output. Default: $YEAR/input/day-$DAY.txt.
 #          May be useful to set it to '/deb/stdout'.
+# - SAMPLE -- try to extract sample input from the text of the problem. Default: unset,
+#             which means False. If set, means True and overrides the main mode.
+#             Doesn't need a session file. Print result to stdout.
+#             This mode requires Haskell toolchain (GHC+Cabal) to parse HTML.
 #
 set -e
 
-: "${SESSION:=./session}"
 : "${YEAR:=$(date +%Y)}"
 : "${DAY:=$(date +%-d)}"
+
+if [ -v SAMPLE ]; then
+  ME="$(realpath "${BASH_SOURCE[-1]}")"
+  DIR="$(dirname "$ME")"
+  wget -q -O - https://adventofcode.com/"$YEAR"/day/"$DAY" | $DIR/extract-html-code.hs
+  exit 0;
+fi
+
 : "${OUT:=$YEAR/input/day-$DAY.txt}"
+: "${SESSION:=./session}"
 SESSIONKEY=$(cat "$SESSION")
 # echo "DAY=$DAY, YEAR=$YEAR"
 curl --cookie "session=$SESSIONKEY" https://adventofcode.com/"$YEAR"/day/"$DAY"/input > "$OUT"
