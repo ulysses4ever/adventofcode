0e3504f6d89e457a6a0404ff49264757f5aa3657
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2023-12-01 17:59:35 -0500

    y2023/d1/p2 fail

--- Y2023/day-1.hs
+++ Y2023/day-1.hs
@@ -1,20 +1,47 @@
 module Main where
 
 import Data.Char
+import Data.List (foldl', isPrefixOf)
 
 import AoC
 
 solve :: [String] -> Int -> Int
 solve inp = \case
     1 -> part1 inp
-    2 -> 0
+    2 -> inp |> part2 .> part1
   where
     part1 = map getDigit .> sum
     getDigit :: String -> Int
     getDigit
       =  filter isDigit
       .> (\ds -> [head ds, last ds])
-      .> (\i -> trace i $ read i)
+      .> read
+
+    part2 :: [String] -> [String]
+    part2 = map digitize
+
+digitize :: String -> String
+digitize = \case
+  [] -> []
+  ln -> let (c:ln') = tryDigitizePrefix ln in
+     c : digitize ln'
+
+tryDigitizePrefix :: String -> String
+tryDigitizePrefix ln = foldl' tryPrefix ln [
+          ("one","1")
+          , ("two","2")
+          , ("three","3")
+          , ("four","4")
+          , ("five","5")
+          , ("six","6")
+          , ("seven","7")
+          , ("eight","8")
+          , ("nine","9")
+          ]
+tryPrefix :: String -> (String, String) -> String
+tryPrefix ln (w, d)
+  | w `isPrefixOf` ln = head d : drop (length w) ln
+  | otherwise = ln
 
 main :: IO ()
 main = defaultMain solve
