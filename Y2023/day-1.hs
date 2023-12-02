9a388d521a2279096d068fa71ef495dd7a7413be
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2023-12-01 21:30:26 -0500

    y2023/d1/p2

--- Y2023/day-1.hs
+++ Y2023/day-1.hs
@@ -11,14 +11,15 @@ solve inp = \case
     2 -> inp |> part2 .> part1
   where
     part1 = map getDigit .> sum
-    getDigit :: String -> Int
-    getDigit
-      =  filter isDigit
-      .> (\ds -> [head ds, last ds])
-      .> read
 
     part2 :: [String] -> [String]
     part2 = map digitize
+
+getDigit :: String -> Int
+getDigit
+  =  filter isDigit
+  .> (\ds -> [head ds, last ds])
+  .> read
 
 digitize :: String -> String
 digitize = \case
@@ -27,21 +28,22 @@ digitize = \case
      c : digitize ln'
 
 tryDigitizePrefix :: String -> String
-tryDigitizePrefix ln = foldl' tryPrefix ln [
-          ("one","1")
-          , ("two","2")
-          , ("three","3")
-          , ("four","4")
-          , ("five","5")
-          , ("six","6")
-          , ("seven","7")
-          , ("eight","8")
-          , ("nine","9")
-          ]
-tryPrefix :: String -> (String, String) -> String
-tryPrefix ln (w, d)
-  | w `isPrefixOf` ln = head d : drop (length w) ln
-  | otherwise = ln
+tryDigitizePrefix ln = foldl' tryDigitizeWith ln
+  [ ("one",'1')
+  , ("two",'2')
+  , ("three",'3')
+  , ("four",'4')
+  , ("five",'5')
+  , ("six",'6')
+  , ("seven",'7')
+  , ("eight",'8')
+  , ("nine",'9')
+  ]
+
+tryDigitizeWith :: String -> (String, Char) -> String
+tryDigitizeWith ln (w, d)
+  | w `isPrefixOf` ln = d : tail ln
+  | otherwise         = ln
 
 main :: IO ()
 main = defaultMain solve
