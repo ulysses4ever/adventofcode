55766384e1b3d1c6f26c6ec2d1a0cd32db5c2818
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2023-12-03 17:28:50 -0500

    tmp

--- Y2023/day-3.hs
+++ Y2023/day-3.hs
@@ -9,6 +9,7 @@ import Data.Char (isDigit)
 import Control.Monad.Trans.Writer.CPS (runWriter, tell, censor)
 import Algorithm.Search (dfsM, pruning)
 import Data.Maybe (maybeToList)
+import System.IO.Unsafe (unsafePerformIO)
 
 type M = Map P Char
 type C = (M, Int, Int)
@@ -25,20 +26,13 @@ solve inp = -- traceShow rs $
     part2 = 0
 
 dfsRoot :: C -> P -> [P]
-dfsRoot (m, rmx, cmx) r = traceShow (init r) res
+dfsRoot c r = traceShow (init r) res
   where
     res = concatMap myDfs (init r)
     myDfs :: P -> [P]
     myDfs init =
-      let (_, res) = runWriter $ dfsM next (const False .> pure) init
+      let (_, res) = runWriter $ dfsM (next c) (const False .> pure) init
       in res
-    --next :: P -> [P]
-    -- next :: _
-    next = eps `pruning` outofbound `pruning` ((`M.lookup` m) .> \case
-                                               Nothing -> True
-                                               Just c  -> not $ isDigit c
-                                           )
-                    .> (\ps -> traceShow ps $ censor (ps++) (pure ps))
     init q@(P r c)
       = [ p
         | dr <- [-1..1]
@@ -50,6 +44,14 @@ dfsRoot (m, rmx, cmx) r = traceShow (init r) res
         , c <- maybeToList $ p `M.lookup` m
         , isDigit c
         ]
+    --next :: P -> [P]
+    -- next :: _
+next (m,rmx,cmx) = eps `pruning` outofbound `pruning` ((`M.lookup` m) .> \case
+                                            Nothing -> True
+                                            Just c  -> not $ isDigit c
+                                        )
+                .> (\ps -> traceShow ps $ censor (ps++) (pure ps))
+  where
     eps (P r c) = [P r (c-1), P r (c+1)]
     outofbound (P r c)
       =  not
@@ -74,5 +76,8 @@ parse inp = (M.fromList psAll, length inp, length $ head inp)
             | x == '.' = ps
             | otherwise = (P r c, x) : ps
 
+tiny = ["*12"]
+c=parse tiny
+
 main :: IO ()
-main = defaultMain solve
+main = print $ next c (P 0 1) --defaultMain solve
