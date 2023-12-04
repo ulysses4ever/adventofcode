5a41d03f7200f2bfa035c6884776292acffb4c0c
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2023-12-03 21:55:54 -0500

    tmp

--- Y2023/day-3.hs
+++ Y2023/day-3.hs
@@ -4,12 +4,12 @@ import AoC
 
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as M
-import Data.List (foldl')
+import Data.List (groupBy, foldl', sort)
+import Data.Containers.ListUtils (nubOrd)
 import Data.Char (isDigit)
 import Control.Monad.Trans.Writer.CPS (runWriter, tell, censor)
 import Algorithm.Search (dfsM, pruning)
-import Data.Maybe (maybeToList)
-import System.IO.Unsafe (unsafePerformIO)
+import Data.Maybe (maybeToList, fromJust)
 
 type M = Map P Char
 type C = (M, Int, Int)
@@ -17,22 +17,30 @@ type C = (M, Int, Int)
 solve :: [String] -> Int -> Int
 solve inp = -- traceShow rs $
   \case
-    1 -> 0 -- traceShow part1 0
+    1 -> traceShow nums (traceShow digs part1)
     2 -> part2
   where
     c@(m, rmx, cmx) = parse inp
     rs = roots m
-    part1 = dfsRoot c (head rs)
+    digs = sort $ nubOrd $ foldl' f [] rs
+      where
+        f ds p = ds ++ dfsRoot c p
+    nums :: [Int]
+    nums
+      =  digs
+      |> groupBy (\(P r1 c1) (P r2 c2) -> r1 == r2 && abs (c2 - c1) == 1)
+      .> map (map ((`M.lookup` m) .> fromJust) .> read)
+    part1 = sum nums
     part2 = 0
 
 dfsRoot :: C -> P -> [P]
-dfsRoot c r = traceShow (init r) res
+dfsRoot c@(m,rmx,cmx) r = res -- traceShow (init r) res
   where
-    res = concatMap myDfs (init r)
+    res = nubOrd $ concatMap myDfs (init r)
     myDfs :: P -> [P]
     myDfs init =
       let (_, res) = runWriter $ dfsM (next c) (const False .> pure) init
-      in res
+      in init : res
     init q@(P r c)
       = [ p
         | dr <- [-1..1]
@@ -43,20 +51,21 @@ dfsRoot c r = traceShow (init r) res
         , p /= q
         , c <- maybeToList $ p `M.lookup` m
         , isDigit c
-        ]
-    --next :: P -> [P]
-    -- next :: _
-next (m,rmx,cmx) = eps `pruning` outofbound `pruning` ((`M.lookup` m) .> \case
+        ] |> filter (outofbound rmx cmx .> not)
+
+next (m,rmx,cmx) = eps
+  `pruning` outofbound rmx cmx
+  `pruning` ((`M.lookup` m) .> \case
                                             Nothing -> True
                                             Just c  -> not $ isDigit c
                                         )
-                .> (\ps -> traceShow ps $ censor (ps++) (pure ps))
+                .> (\ps -> censor (ps++) (pure ps))
   where
     eps (P r c) = [P r (c-1), P r (c+1)]
-    outofbound (P r c)
-      =  not
-      $  0 <= r && r < rmx
-      && 0 <= c && c < cmx
+outofbound rmx cmx (P r c)
+  =  not
+  $  0 <= r && r < rmx
+  && 0 <= c && c < cmx
 
 roots :: M -> [P]
 roots = M.foldrWithKey f []
@@ -76,8 +85,14 @@ parse inp = (M.fromList psAll, length inp, length $ head inp)
             | x == '.' = ps
             | otherwise = (P r c, x) : ps
 
-tiny = ["*12"]
+tiny =
+  [   ".3....."
+    , ".*12..."
+    , ".34...."]
 c=parse tiny
 
 main :: IO ()
-main = print $ next c (P 0 1) --defaultMain solve
+main =
+  -- print $ dfsRoot c (P 1 1)
+  -- runWriter $ dfsM (next c) (const False .> pure) (P 1 1) --
+  defaultMain solve
