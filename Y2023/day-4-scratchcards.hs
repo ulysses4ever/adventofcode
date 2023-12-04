435ad192760b1f6e5a3d46f7e6853168815bc725
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2023-12-04 12:11:06 -0500

    y2023/d4/p1

--- Y2023/day-4-scratchcards.hs
+++ Y2023/day-4-scratchcards.hs
@@ -2,12 +2,23 @@ module Main where
 
 import AoC
 
+import Data.List (intersect)
+
 solve :: [String] -> Int -> Int
 solve inp = \case
     1 -> part1
     2 -> part2
   where
-    part1 = 0
+    wsmy' :: [([Int],[Int])]
+    wsmy' = do
+      wsmy <- inp
+      let (ws, my) = span (/= '|') wsmy
+      pure (tail $ input ws, input my)
+
+    score (ws, my) = let
+      exp = (length (ws `intersect` my) - 1)
+      in if exp < 0 then 0 else 2^exp
+    part1 = map score wsmy' |> sum -- (traceShowId $ map score (traceShowId wsmy'))
     part2 = 0
 
 main :: IO ()
