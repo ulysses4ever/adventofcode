502163a9fc1b8f2460c0566b3b6ef25709c72581
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2023-12-07 11:50:49 -0500

    y2023/d7/p1 start

--- Y2023/day-7-cards.hs
+++ Y2023/day-7-cards.hs
@@ -4,6 +4,7 @@ import Data.List (sort, group)
 
 import AoC
 import Data.Char (isDigit, ord)
+import Data.List (sortOn)
 
 data C =
     C2 | C3 | C4 | C5 | C6 | C7 | C8 | C9 | T | J | Q | K | A
@@ -24,5 +25,31 @@ solve inp = \case
     part1 = pTraceShowCompact hsbs 0
     part2 = 0
 
+data S =
+    HH | P1 | P2 | K3 | FH | K4 | K5
+    deriving (Eq, Ord, Bounded, Enum, Show, Read)
+
+scoreHand :: [C] -> S
+scoreHand h = case h |> sort .> group .> sortOn length of
+    [a] -> K5
+    [a,b] -> case length a of
+      1 -> K4
+      2 -> FH
+    [a,b,c] -> case length c of
+      3 -> K3
+      2 -> P2
+    [a,b,c,d] -> P1
+    _ -> HH
+
 main :: IO ()
 main = defaultMain solve
+
+{-
+i = [
+    ( [ C3, C2, T, C3, K ], 765 ),
+    ( [ T, C5, C5, J, C5 ], 684 ),
+    ( [ K, K, C6, C7, C7 ], 28 ),
+    ( [ K, T, J, J, T ], 220 ),
+    ( [ Q, Q, Q, J, A ], 483 )
+    ]
+-}
