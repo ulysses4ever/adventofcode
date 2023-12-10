f2b16fe2fa75b1ad9994814bca0a9d0d38c85bbc
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2023-12-09 21:32:11 -0500

    y2023/d7/p2 CPP!!!11

--- Y2023/day-7-cards.hs
+++ Y2023/day-7-cards.hs
@@ -1,38 +1,53 @@
+{-# language CPP #-}
 module Main where
 
-import Data.List (sort, group)
+import Data.List ( sort, group, sortOn, sortBy )
 
 import AoC
 import Data.Char (isDigit, ord)
-import Data.List (sortOn, sortBy)
 import Data.Ord
 
+#define p2
+
 data C =
+#ifndef p2
     C2 | C3 | C4 | C5 | C6 | C7 | C8 | C9 | T | J | Q | K | A
-    deriving (Eq, Ord, Bounded, Enum, Show, Read)
+#else
+    J | C2 | C3 | C4 | C5 | C6 | C7 | C8 | C9 | T | Q | K | A
+#endif
+    deriving (Eq, Ord, Enum, Show, Read)
 
 readCard :: Char -> C
 readCard c
   | isDigit c = iterate succ C2 !! (ord c - ord '2')
   | otherwise = read [c]
 
+type H = [C]
+
+readHand :: String -> H
+readHand = map readCard
+
 solve :: [String] -> Int -> Int
-solve inp = \case
+solve inp p = case p of
     1 -> part1
     2 -> part2
   where
-    hsbs :: [([C], Int)]
-    hsbs = map (words .> \[h, b] -> (map readCard h, read b)) inp
-    part1 -- pTraceShowCompact (map (\(h,_)->(h, scoreHand h)) hsbs) 0
+    hsbs :: [((H, S), Int)]
+    hsbs = map (words .> \[h, b] -> let h' = readHand h in ((h', sch h'), read b)) inp
+    sch | p == 1 = scoreHand
+        | otherwise = scoreHand'
+    res
       =  hsbs |> sortBy (\(h1,_) (h2,_) -> cmpHands h1 h2)
       .> zip [1..]
       .> map (\(i,(_,b)) -> i*b)
       .> sum
-    part2 = 0
+
+    part1 = res
+    part2 = res
 
 data S =
     HH | P1 | P2 | K3 | FH | K4 | K5
-    deriving (Eq, Ord, Bounded, Enum, Show, Read)
+    deriving (Eq, Ord, Enum, Show, Read)
 
 scoreHand :: [C] -> S
 scoreHand h = case h |> sort .> group .> sortOn length of
@@ -46,25 +61,24 @@ scoreHand h = case h |> sort .> group .> sortOn length of
     [a,b,c,d] -> P1
     _ -> HH
 
-cmpHands :: [C] -> [C] -> Ordering
-cmpHands h1 h2
+-- assumes hand is sorted
+scoreHand' :: H -> S
+scoreHand' = sort .> variate .> map scoreHand .> maximum
+
+variate :: H -> [H]
+variate h = go h
+  where
+    go h' = case h' of
+        []   -> [[]]
+        J:h'' -> h >>= (\c -> map (c:) $ variate h'')
+        _    -> pure h
+
+cmpHands :: (H, S) -> (H, S) -> Ordering
+cmpHands (h1, s1) (h2, s2)
   | h1 == h2 = EQ
   | s1 == s2 = compare h1 h2
-  | otherwise = comparing scoreHand h1 h2
-  where
-    s1 = scoreHand h1
-    s2 = scoreHand h2
-
+  | otherwise = compare s1 s2
 
 main :: IO ()
 main = defaultMain solve
-
-{-
-i = [
-    ( [ C3, C2, T, C3, K ], 765 ),
-    ( [ T, C5, C5, J, C5 ], 684 ),
-    ( [ K, K, C6, C7, C7 ], 28 ),
-    ( [ K, T, J, J, T ], 220 ),
-    ( [ Q, Q, Q, J, A ], 483 )
-    ]
 -}
