c753bd4c551aa3063b565127f6df9ddc3a38914f
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2024-11-28 14:43:19 -0500

    y2023/d8/p1 optimized but still not working

--- Y2023/day-8-maps.hs
+++ Y2023/day-8-maps.hs
@@ -1,6 +1,9 @@
 module Main where
 
+import Data.IntMap.Strict (IntMap)
+import qualified Data.IntMap.Strict as IntMap
 import Data.List (find)
+import Data.Char
 import AoC
 
 solve :: Groups (Lines (Words String)) -> Int -> Int
@@ -15,12 +18,25 @@ solve (Gs [_, (Ls ls')]) = \case
     db = flip map ls \[fst, _eq, snd, trd] ->
       (fst, [tail $ init snd, init trd])
 
-    part1 = iterate (step db) "ZZZ" |> takeWhile (/= "AAA") |> length
+    db1 = IntMap.fromList
+      $ filter (\(a,b) -> a /= b)
+      $ flip concatMap db \(f,[s,t]) ->
+        [(c s, c f), (c t,c f)]
+
+    part1 = --pTraceShow db1
+      (iterate (db1 IntMap.!) (c "ZZZ")
+      |> takeWhile (/= c "AAA")
+      |> length)
 
     part2 = 0
 
 step db needle = (flip find db \(f,st) ->
   needle `elem` st) |> (\(Just (f,_)) -> f)
+
+c :: String -> Int
+c s = zipWith (*) [2,3,5] (map i s) |> sum
+  where
+    i c = ord c - ord 'A'
 
 main :: IO ()
 main = defaultMain solve
