969c140e0c266017f6152d9172587ee7ee185ad4
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2024-11-28 15:51:03 -0500

    y2023/d8/p1 done
    I wasn't encoding it right -- so embarrassing!..
    
    instead of
    
      abc -> 2*a + 3*b + 5*c
    
    should do
    
      abc -> a + 26*b + 26^2*c

--- Y2023/day-8-maps.hs
+++ Y2023/day-8-maps.hs
@@ -2,39 +2,44 @@ module Main where
 
 import Data.IntMap.Strict (IntMap)
 import qualified Data.IntMap.Strict as IntMap
+import Data.IntMap.Strict ((!))
 import Data.List (find)
 import Data.Char
 import AoC
 
-solve :: Groups (Lines (Words String)) -> Int -> Int
-solve (Gs [_, (Ls ls')]) = \case
+solve :: (String, Lines (Words String)) -> Int -> Int
+solve (cs, (Ls ls')) = \case
     1 -> part1
     2 -> part2
   where
     ls :: [[String]]
     ls = coerce ls'
 
-    db :: [(String,[String])]
-    db = flip map ls \[fst, _eq, snd, trd] ->
-      (fst, [tail $ init snd, init trd])
+    db :: IntMap (Int,Int,String)
+    db = IntMap.fromList $
+      flip map ls \[fst, _eq, snd, trd] ->
+        (c fst, (c $ tail $ init snd, c $ init trd, fst))
 
-    db1 = IntMap.fromList
-      $ filter (\(a,b) -> a /= b)
-      $ flip concatMap db \(f,[s,t]) ->
-        [(c s, c f), (c t,c f)]
-
-    part1 = --pTraceShow db1
-      (iterate (db1 IntMap.!) (c "ZZZ")
-      |> takeWhile (/= c "AAA")
-      |> length)
+    part1 =
+      interp db (cycle cs) 0 (c "AAA")
 
     part2 = 0
+
+zzz = c "ZZZ"
+
+interp db (c:cs) index state
+  | state == zzz = index
+  | c == 'L' = next l
+  | c == 'R' = next r
+  where
+    (l, r, lbl) = db ! state
+    next = interp db cs (index+1)
 
 step db needle = (flip find db \(f,st) ->
   needle `elem` st) |> (\(Just (f,_)) -> f)
 
 c :: String -> Int
-c s = zipWith (*) [2,3,5] (map i s) |> sum
+c s = zipWith (*) [1,26,26*26] (map i s) |> sum
   where
     i c = ord c - ord 'A'
 
