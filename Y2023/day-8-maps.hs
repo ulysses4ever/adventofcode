3aa34850b6dd243e1c5aaee3e0e961875972a0f6
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2024-11-28 22:13:45 -0500

    y2023/d8/p2 fail

--- Y2023/day-8-maps.hs
+++ Y2023/day-8-maps.hs
@@ -9,8 +9,8 @@ import AoC
 
 solve :: (String, Lines (Words String)) -> Int -> Int
 solve (cs, (Ls ls')) = \case
-    1 -> part1
-    2 -> part2
+    1 -> 0 -- part1
+    2 -> pTraceShowCompact db part2
   where
     ls :: [[String]]
     ls = coerce ls'
@@ -20,10 +20,41 @@ solve (cs, (Ls ls')) = \case
       flip map ls \[fst, _eq, snd, trd] ->
         (c fst, (c $ tail $ init snd, c $ init trd, fst))
 
-    part1 =
+    part1' =
       interp db (cycle cs) 0 (c "AAA")
 
-    part2 = 0
+    part1 =
+      iterateWhile
+        (\(_db, _cs, node, _index) -> node == zzz)
+        (\(db, (c:cs), node, !index) -> let
+               (l,r, _lbl) = db ! node
+               node' = case c of
+                        'L' -> l
+                        'R' -> r
+            in (db, cs, node', index+1)
+           )
+        (db, cycle cs, c "AAA", 0)
+      |> (\(_,_,_,index) -> index)
+
+    endsWith c n =
+      db ! n |> (\(_,_,lbl) -> last lbl == c)
+
+    part2 = pTraceShowCompact init
+      (iterateWhile
+        (\(_cs, nodes, _index) -> all (endsWith 'Z') nodes)
+        (\((c:cs), nodes, !index) -> let
+               step node = let
+                    (l,r, _lbl) = db ! node
+                 in case c of
+                        'L' -> l
+                        'R' -> r
+            in (cs, map step nodes, index+1)
+           )
+        (cycle cs, init, 0)
+      |> (\(_,_,index) -> index))
+      where
+        init = IntMap.filter (\(_,_,lbl) -> last lbl == 'A') db
+          |> IntMap.keys
 
 zzz = c "ZZZ"
 
@@ -35,8 +66,15 @@ interp db (c:cs) index state
     (l, r, lbl) = db ! state
     next = interp db cs (index+1)
 
-step db needle = (flip find db \(f,st) ->
-  needle `elem` st) |> (\(Just (f,_)) -> f)
+iterateWhile ::
+  (state -> Bool) ->
+  (state -> state) ->
+  state ->
+  state
+iterateWhile isFinal next current
+  | isFinal current = current
+  | otherwise = iterateWhile isFinal next (next current)
+ 
 
 c :: String -> Int
 c s = zipWith (*) [1,26,26*26] (map i s) |> sum
