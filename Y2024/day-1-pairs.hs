03415054243e6cb24295a7ca87313427900a9576
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2024-12-01 21:58:47 -0500

    y2023/d1 prettify: use `counter` and use Input class wisely
    for the latter: just `[[Int]]` works

--- Y2024/day-1-pairs.hs
+++ Y2024/day-1-pairs.hs
@@ -2,18 +2,16 @@ module Main where
 
 import AoC
 import Data.List (transpose, sort)
+import qualified Data.IntMap.Strict as IntMap
 
-solve :: Lines [Int] -> Int -> Int
-solve (Ls ws) = \case
-    1 -> part1
-    2 -> part2
+solve :: [[Int]] -> Int -> Int
+solve rows = \case
+    1 -> zipWith (\a b -> abs $ a - b) (sort ls) (sort rs) |> sum
+    2 -> map f ls |> sum
   where
-    [ls, rs] = transpose ws
-
-    part1 = zipWith (\a b -> abs $ a - b) (sort ls) (sort rs) |> sum
-    part2 = foldl' f 0 ls
-
-    f sum x = x * (filter (==x) rs |> length) + sum
+    _cols@[ls, rs] = transpose rows
+    crs = counter rs
+    f x = x * IntMap.findWithDefault 0 x crs
 
 main :: IO ()
 main = defaultMain solve
