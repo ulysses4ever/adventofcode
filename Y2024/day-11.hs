66b471ab8b4ddca496eb00a2b1b7fd0a847b70f8
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2024-12-11 09:59:26 -0500

    y2024/d11/p2 required switching from list to counting map
    Obvious optimization when the order of datapoints isn't relevant.

--- Y2024/day-11.hs
+++ Y2024/day-11.hs
@@ -1,14 +1,31 @@
 module Main (main) where
 
 import AoC
+import qualified Data.IntMap.Strict as IntMap
+import Data.IntMap.Strict (IntMap)
+
+type StoneCounts = IntMap Int
 
 solve :: [Int] -> Int -> Int
 solve inp = \case
-    1 -> res 25
-    2 -> 0--res 75
+    1 -> res' 25
+    2 -> res' 75
   where
     res steps = iterate blink inp !! steps |> length
+    res' steps = iterate blink' stoneCounts !! steps |> IntMap.foldl' (+) 0
 
+    stoneCounts = counter inp
+
+blink' :: StoneCounts -> StoneCounts
+blink' = IntMap.foldlWithKey' f IntMap.empty
+  where
+    f accCounts stone count =
+      foldl'
+        (\accCounts' newStone -> IntMap.insertWith (+) newStone count accCounts')
+        accCounts
+        (updateStone stone)
+
+-- This worked for part 1 but not part 2, which runs out of time
 blink :: [Int] -> [Int]
 blink = concatMap updateStone
 
