bbbcebaf315043a785bda09c3b7d9a233d037c4d
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2025-12-15 12:30:43 -0500

    y2025/d1/p1

--- Y2025/day-1-dial.hs
+++ Y2025/day-1-dial.hs
@@ -1,21 +1,21 @@
-
 module Main where
-
 
 import AoC
 import Data.List (transpose, sort)
 
 solve :: [String] -> Int -> Int
-solve inp _n = foldr step (50, 0) inp |> snd
+solve inp _n = foldl' step (50, 0) inp |> snd
   where
-    step (d:n) (acc, res) =
+    step (acc, res) (d:n) =
       let
-        n' = read n `mod` 100
+        n' = read n
         acc' = case d of
                  'L' -> (acc - n' + 100) `mod` 100
                  'R' -> (acc + n') `mod` 100
-      in (traceShowId acc', upd acc' res)
+      in (acc', upd acc' res)
+
     upd 0 res = res + 1
     upd _ res = res
+
 main :: IO ()
 main = defaultMain solve
