f2228116e56f86f6e1bf4ae2dc2f8c687f95489f
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2025-12-16 11:15:04 -0500

    y2025/d1/p2 incorrect

--- Y2025/day-1-dial.hs
+++ Y2025/day-1-dial.hs
@@ -4,9 +4,13 @@ import AoC
 import Data.List (transpose, sort)
 
 solve :: [String] -> Int -> Int
-solve inp _n = foldl' step (50, 0) inp |> snd
+solve inp = \case
+  1 -> foldRes step1
+  2 -> foldRes step2
   where
-    step (acc, res) (d:n) =
+    foldRes f = foldl' f (50, 0) inp |> snd
+
+    step1 (acc, res) (d:n) =
       let
         n' = read n
         acc' = case d of
@@ -16,6 +20,15 @@ solve inp _n = foldl' step (50, 0) inp |> snd
 
     upd 0 res = res + 1
     upd _ res = res
+
+    step2 (acc, res) (d:n) =
+      let
+        n' = read n
+        acc' = case d of
+                 'L' -> (acc - n')
+                 'R' -> (acc + n')
+        (dv, md) = acc' `divMod` 100
+      in (md, res + abs dv)
 
 main :: IO ()
 main = defaultMain solve
